a50b4a964344b3b16ce6ad57f1c732265cd9cc54
sadatowrustam <rustamsadatov0.com>
2024-04-29 15:12:44 +0500

    otkat 2

--- src/admin/controllers/gallery.controller.ts
+++ src/admin/controllers/gallery.controller.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Delete, Get,HttpCode,Param, ParseIntPipe, Post, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
+import { Controller, Delete, Get,HttpCode,Param, ParseIntPipe, Post,  UploadedFiles, UseGuards, UseInterceptors } from '@nestjs/common';
 import { JwtGuard } from '../guard'
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { GalleryService } from '../services/gallery.service';
@@ -18,7 +18,7 @@ export class GalleryController {
   @HttpCode(200) 
   @Post()
   @UseInterceptors(FilesInterceptor("images")) 
-   async uploadImage(@UploadedFile() file:Express.Multer.File[]){
+   async uploadImage(@UploadedFiles() file:Express.Multer.File[]){
      return this.galleryService.uploadImage(file)
   }
   @Delete(":id")
