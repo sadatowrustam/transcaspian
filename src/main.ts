fc056adb84294d6f4d6ea41f8ce1d936b872a504
sadatowrustam <rustamsadatov0@gmail.com>
2024-05-23 13:34:15 +0500

    big text fix

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as express from 'express';
+import { json } from 'express';
+import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
+  app.use(json({ limit: '50mb' }));
   app.use(express.static('src/uploads'));
   await app.listen(5014);
 }
